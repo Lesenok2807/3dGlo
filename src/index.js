ff6cbcd531d37eb43527834d26925e40bebe063f
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-10-04 22:37:19 +0300

    первый коммит 24 урока

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import modal from './modules/modal';
 import calc from './modules/calc';
 import form from './modules/form';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 
 timer('11 october 2023');
 menu();
 modal();
-calc();
+calc(100);
 form();
 tabs();
+slider();
